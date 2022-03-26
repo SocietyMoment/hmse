20210e67d31b4262f1a58c7f79f3f359330703ee
SocietyMoment <societymoment@protonmail.com>
2022-03-25 21:47:43 -0400

    dozzle

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import os
 from typing import Optional
 from flask import Blueprint
-from jinja2 import Markup
+from markupsafe import Markup
 import posix_ipc
 
 utils_bp = Blueprint('utils', __name__)
